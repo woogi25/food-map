240aaf039da1fc954a26fc86e5f666ff64d057fe
woogi25 <hyunwook1891@gmail.com>
2022-11-22 12:15:28 +0900

    63강 회원가입 API 구현 공부

--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -1,5 +1,26 @@
 const { pool } = require("../../config/database");
 
+// 회원가입
+exports.insertUsers = async function (connection, userID, password, nickname) {
+  const Query = `insert into Users(userID, password, nickname) values (?,?,?);`;
+  const Params = [userID, password, nickname];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+// 로그인 (회원검증)
+exports.isValidUsers = async function (connection, userID, password) {
+  const Query = `SELECT userIdx, nickname FROM Users where userID = ? and password = ? and status = 'A';`;
+  const Params = [userID, password];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+
 exports.selectRestaurants = async function (connection,category) {
   const selectAllRestaurantsQuery = `select title, address, category, videoUrl 
   from Restaurants where status = 'A';`;
